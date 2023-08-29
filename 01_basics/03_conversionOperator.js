d364795fbde098854a53bce5dfe48475cf27e50c
Sumedh Barsagade <66020343+sumedhnbarsagade@users.noreply.github.com>
2023-08-29 06:45:10 +0000

    conversion

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,15 @@
+let score = "33avb";
+
+//console.log(typeof score);
+//console.log(typeof (score))
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// 
+
+let isLoggedIn = 1;
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn)
