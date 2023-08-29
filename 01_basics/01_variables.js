7b0607d0ee3d0c39905d88b2cc3234aa3eb48515
Sumedh Barsagade <66020343+sumedhnbarsagade@users.noreply.github.com>
2023-08-29 06:28:50 +0000

    datatypes in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123;
+let accountEmail = "sumedh@gmail.com";
+var accountPassword = "Sumdh@123";
+accountCity = "Nagpur";
+let accountState;
+
+/*
+do not use var for keyword declaration in javascript
+when you not give value to any variable its bydefault value should be undefined.
+*/
+accountEmail = "hdfc@gmail.com";
+accountPassword = "246746";
+accountCity = "Pune";
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity, accountState] );
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Sumedh";
+let age = 23;
+let isLoggedIn = false;
+
+/**
+ * number => 2 to power 53
+ * bigint
+ * string => ""
+ * boolen => true.false
+ * null => standalone
+ * undefined
+ * symbole => unique
+ * 
+ * object
+ */
+
+console.log(typeof age);
+console.log(typeof undefined);
