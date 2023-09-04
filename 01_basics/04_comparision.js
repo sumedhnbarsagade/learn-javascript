ffc7264810f455726b83fc81053115bfa31f07d8
Sumedh Barsagade <66020343+sumedhnbarsagade@users.noreply.github.com>
2023-09-04 11:04:51 +0000

    comparision operators in javascript

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -4,8 +4,20 @@
 // console.log(2 >= 1)
 // console.log(2 !=  2)
 // console.log(2==2)
-
+// console.log(2**3) power
+// console.log(2%3) reminder
 
 // ===   -- strict check
 // == -- 
 
+let Name = "Sumedh";
+let surName = " Barsagade";
+
+let fullName = Name + surName;
+
+//console.log(fullName);
+
+let gameCounter = 100;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
