adec01ed0d1e14f5a538af34624b6d384b29ace8
Sumedh Barsagade <66020343+sumedhnbarsagade@users.noreply.github.com>
2023-12-15 15:53:07 +0000

    Conversion of datatype

---
+++ New/01_basics/03_conversionOperator.js
@@ -0,0 +1,34 @@
+let score = undefined;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// Hpw to change the datatype
+
+let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber));
+
+//console.log(valueInNumber);
+
+/**
+ * "33" => 33
+ * "33abc" => NaN
+ * true => 1
+ * null = 0
+ */
+
+
+let isLoggedIn = "Sumedh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 = > true
+// 0 -> false
+// "" => false
+// "Sumedh"=> true
+
+let myNewNumber = 34;
+let stringNumber = String(myNewNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
