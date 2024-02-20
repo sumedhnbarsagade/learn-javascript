f78546056a9d9adaa7e9a3a8a00fbaf8db7d3a23
Sumedh Barsagade <66020343+sumedhnbarsagade@users.noreply.github.com>
2024-02-20 17:43:14 +0000

    learn new things in object

---
+++ 03_object/03_object.js
@@ -0,0 +1,33 @@
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+//const obj3 = {obj1, obj2}
+// copy object from 1 to another
+//const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+//const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "sumedh@gmail.com"
+    },
+    {
+        id: 2,
+        email: "sumedh@gmail.com"
+    },
+    {
+        id: 3,
+        email: "sumedh@gmail.com"
+    },
+    {
+        id: 4,
+        email: "sumedh@gmail.com"
+    },
+]
+
+
+// users[1].email
---
+++ 03_object/04_object.js
@@ -0,0 +1,12 @@
+const course ={
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Sumedh"
+}
+
+
+const {courseInstructor: instructor} = course;
+
+//console.log(courseInstructor);
+
+console.log(instructor);
