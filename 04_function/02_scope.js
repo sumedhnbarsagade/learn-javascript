46c6b7d46a86338a187aeae652b803606d5b07ce
Sumedh Barsagade <66020343+sumedhnbarsagade@users.noreply.github.com>
2024-02-21 13:29:40 +0000

    scope variable in javascript

---
+++ 04_function/02_scope.js
@@ -0,0 +1,31 @@
+
+if (true){
+    let a = 10;
+
+    const b = 20;
+    
+    var c = 30;  // not use this
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "sumedh";
+
+    function two(){
+        const website = "icedq.com"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two();
+}
+one()
+
+
+if (true){
+    const username = "hitesh"
+    
+}
